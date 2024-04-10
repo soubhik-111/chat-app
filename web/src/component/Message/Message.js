4764de6d909f181edd62c25db944022dede39a30
soubhik-111 <soubhikbanerjee04@gmail.com>
2024-04-10 15:28:33 +0530

    Enhancement UI message & added copy

--- web/src/component/Message/Message.js
+++ web/src/component/Message/Message.js
@@ -1,21 +1,33 @@
 import React from 'react';
 import "./Message.css";
+import copyIcon from "../../image/copy-icon.png"
 
-const Message = ({user,message,classs}) => {
-    if(user){
-        return (
-            <div className={`MessageBox ${classs}`}>
-              {`${user}: ${message}`}
-            </div>
-          )
-    }
-
+const Message = ({ user, message, classs }) => {
+  const handleCopy = () => {
+    navigator.clipboard.writeText(message)
+  }
+  if (user) {
     return (
-        <div className={`MessageBox ${classs}`}>
-          {`You: ${message}`}
+      <div className={`MessageBox ${classs}`}>
+        <div className='MessageHeader'>
+          <p>{user}</p>
+          <img src={copyIcon} alt="copy" className='MessageCopyIcon' onClick={handleCopy} />
         </div>
-      )
-  
+        <div className='Message'>{message}</div>
+      </div>
+    )
+  }
+
+  return (
+    <div className={`MessageBox ${classs}`}>
+      <div className='MessageHeader'>
+        <p>You</p>
+        <img src={copyIcon} alt="copy" style={{ filter: "invert(0)" }} className='MessageCopyIcon' onClick={handleCopy} />
+      </div>
+      <div className='Message'>{message}</div>
+    </div>
+  )
+
 }
 
 export default Message
